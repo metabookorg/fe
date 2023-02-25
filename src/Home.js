75eb2cdca7dac80edbd0c10893ec60a10e9366f0
sbi <luca.mommi@libero.it>
2023-02-25 23:02:07 +0100

    improvements

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,51 @@
 import {useState} from 'react'
 
 const Home = () => {
-    const [book, setBook] = useState('read here')
+    const [book, setBook] = useState([{txt: 'read here'}])
     const [argument, setArgument] = useState('a trip')
     const [environments, setEnvironment] = useState('london')
     const [time, setTime] = useState('industrial revolution')
     const [characters, setCharacters] = useState('sherlock holmes, mammeta')
-    return (
-        <div className='Home'>
-            <input placeholder={argument} onChange={(e) => {
-                setArgument(e.target.value ? e.target.value : argument)
-            }}/>
-            <input placeholder={environments} onChange={(e) => {
-                setEnvironment(e.target.value ? e.target.value : environments)
-            }}/>
-            <input placeholder={time} onChange={(e) => {
-                setTime(e.target.value ? e.target.value : time)
-            }}/>
-            <input placeholder={characters} onChange={(e) => {
-                setCharacters(e.target.value ? e.target.value : characters)
-            }}/>
-            <button
-                onClick={() => submit(argument, environments, time, characters)
-                    .then(res => res.json().then(json => {
-                            let str = ''
-                            for (const page of json['data']) {
-                                str = str + page['idx'] + ' ' + page['txt'] + ' ' + page['url']
-                            }
-                            // should be a list or a slider
-                            setBook(str)
-                        })
-                    )}>submit
-            </button>
-            <br/>
-            <h3>{book}</h3>
-        </div>
-    )
+    return (<div className='Home'>
+        <input placeholder={argument} onChange={(e) => {
+            setArgument(e.target.value ? e.target.value : argument)
+        }}/>
+        <input placeholder={environments} onChange={(e) => {
+            setEnvironment(e.target.value ? e.target.value : environments)
+        }}/>
+        <input placeholder={time} onChange={(e) => {
+            setTime(e.target.value ? e.target.value : time)
+        }}/>
+        <input placeholder={characters} onChange={(e) => {
+            setCharacters(e.target.value ? e.target.value : characters)
+        }}/>
+        <button
+            onClick={() => submit(argument, environments, time, characters)
+                .then(res => res.json()
+                    .then(json => setBook(json['data']))
+                )}>submit
+        </button>
+        <br/>
+        <Book book={book}/>
+    </div>)
+}
+
+const Book = (props) => {
+    let pages = []
+    for (let page of props.book) {
+        // tried to add key prop to list item but didn't work
+        pages.push(<Page txt={page['txt']} url={page['url']}/>)
+    }
+    return (<div className='Book'>
+        <ul>{pages}</ul>
+    </div>)
+}
+
+const Page = (props) => {
+    return (<div className='Page'>
+        <h3>{props.txt}</h3>
+        <img src={props.url} alt=''/>
+    </div>)
 }
 
 const submit = (argument, environments, time, characters) => {
