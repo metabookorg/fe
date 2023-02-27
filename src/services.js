73293549e8aec0cfc3749b4965592a728c429860
sbi <luca.mommi@libero.it>
2023-02-27 18:55:21 +0100

    handling styles

--- src/services.js
+++ src/services.js
@@ -1,4 +1,6 @@
-export const submitWithParams = (argument, environments, time, characters) => {
+const baseUrl = 'http://localhost:1312/metabook'
+
+export const submitWithParams = (argument, environments, time, characters, style) => {
     characters = characters.split(',')
     let body = {
         txt_request: {
@@ -7,23 +9,23 @@ export const submitWithParams = (argument, environments, time, characters) => {
             time: time,
             characters: characters
         },
-        style: 'comic book' // should be taken from be service
+        style: style
     }
-    return fetch('http://localhost:1312/metabook/new/from_params', {
+    return fetch(baseUrl + '/new/from_params', {
         method: 'POST',
         headers: {'content-type': 'application/json'},
         body: JSON.stringify(body)
     })
 }
 
-export const submitWithPrompt = (prompt) => {
+export const submitWithPrompt = (prompt, style) => {
     let body = {
         txt_request: {
             prompt: prompt
         },
-        style: 'comic book' // should be taken from be service
+        style: style
     }
-    return fetch('http://localhost:1312/metabook/new/from_prompt', {
+    return fetch(baseUrl + '/new/from_prompt', {
         method: 'POST',
         headers: {'content-type': 'application/json'},
         body: JSON.stringify(body)
@@ -31,9 +33,15 @@ export const submitWithPrompt = (prompt) => {
 }
 
 export const savePdf = (book) => {
-    return fetch('http://localhost:1312/metabook/export/pdf', {
+    return fetch(baseUrl + '/export/pdf', {
         method: 'POST',
         headers: {'content-type': 'application/json'},
         body: JSON.stringify(book)
+    })
+}
+
+export const getStyles = () => {
+    return fetch(baseUrl + '/static/img_styles', {
+        method: 'GET'
     })
 }
