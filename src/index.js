fb55d60d3b08be57bc670fd2fb6c0c3f12af37f1
sbi <luca.mommi@libero.it>
2023-02-28 19:09:59 +0100

    indent

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from "./Home";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from "./Home"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <Home/>
     </React.StrictMode>
-);
+)
